308e3d456222b4d8c8fa858695dbfad090a46440
Grant Beard <Grantxbeard@gmail.com>
2022-11-21 20:44:08 -0700

    write call function

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const fetchData = (dataSet: string) => {
+    return fetch(`http://localhost:3001/api/v1/${dataSet}`).then((res) => res.json())
+  }
+
+
   return (
     <div>APP</div>
   );
